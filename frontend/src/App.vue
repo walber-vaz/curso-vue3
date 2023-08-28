c6dd74e2abc28671ddf6b9354d74e0956522b6ec
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-08-28 15:08:39 -0300

    feat: v-if, v-else and v-show

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -35,20 +35,60 @@ const users = reactive([
     </nav>
   </header>
 
-  <section>
+  <RouterView class="content" />
+  <section class="section-user">
     <h1>Users</h1>
     <ul>
       <li v-for="user in users" :key="user.id">
         <span v-if="user.role.includes('admin')">👑</span>
-        <strong style="font-weight: bold"
-          >{{ user.name }} - {{ user.age }} - {{ user.email }}</strong
-        >
+        <strong>{{ user.name }} - {{ user.age }} - {{ user.email }}</strong>
       </li>
     </ul>
   </section>
-  <RouterView class="content" />
 </template>
 
 <style lang="scss">
 @import url('@/assets/scss/app.scss');
+
+.section-user {
+  margin-top: 2rem;
+  max-height: 96rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  h1 {
+    font-size: 3.5rem;
+    font-weight: bold;
+    color: #111;
+    margin-bottom: 1rem;
+  }
+
+  ul {
+    display: flex;
+    max-width: 96rem;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    gap: 2rem;
+
+    li {
+      font-size: 2rem;
+      font-weight: bold;
+      color: #111;
+      transition: color 0.2s ease-in-out;
+
+      &:hover {
+        color: #41b883;
+        cursor: pointer;
+      }
+
+      strong {
+        margin-left: 1rem;
+        font-weight: bold;
+      }
+    }
+  }
+}
 </style>
